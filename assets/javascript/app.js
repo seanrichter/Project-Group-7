c383a2020b8b9eef69c9bb4e5d82967b77e84486
Sean Richter <sean.richter04@outlook.com>
2019-09-12 13:47:36 -0500

    added 2 columns for retail and restaurant search parameters.

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+//City-Geo Location Lookup//
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=New%20York",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
+		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+//Yahoo finance api call//
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
