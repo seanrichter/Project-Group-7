1f35c213d2a711ddae7cb118f1798af64d6e5915
Sean Richter <sean.richter04@outlook.com>
2019-09-13 13:06:17 -0500

    added 3 more api's and they are showing json

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-console.log("hello")
 //City-Geo Location Lookup//
 var settings = {
 	"async": true,
@@ -15,14 +14,46 @@ $.ajax(settings).done(function (response) {
 	console.log(response);
 });
 
-//Yahoo finance api call//
+// Yahoo Finance API Documentation//
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en",
+	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/effective-spread",
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-host": "investors-exchange-iex-trading.p.rapidapi.com",
+		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+// Investors Exchange API Documentation//
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/effective-spread",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "investors-exchange-iex-trading.p.rapidapi.com",
+		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+// Crypto Asset Tickers API documentation//
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://bravenewcoin-v1.p.rapidapi.com/ticker?show=usd&coin=btc",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "bravenewcoin-v1.p.rapidapi.com",
 		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
 	}
 }
