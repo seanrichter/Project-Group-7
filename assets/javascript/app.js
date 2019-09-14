574e23430df62c5c83c3ce19befec9f0843da30a
Sean Richter <sean.richter04@outlook.com>
2019-09-14 12:20:58 -0500

    removed old non working api's, got 3 api's that are working to pull data from.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,6 @@
-//City-Geo Location Lookup//
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=New%20York",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
 
 // Yahoo Finance API Documentation//
-var settings = {
+/*var settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/effective-spread",
@@ -28,13 +13,30 @@ var settings = {
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
+}); */
+
+// yahoo financials api documentation 
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=AMRN",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
+
 
 // Investors Exchange API Documentation//
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/effective-spread",
+	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/intc/book",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "investors-exchange-iex-trading.p.rapidapi.com",
@@ -43,10 +45,11 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+	console.log(response.quote.close);
 });
 
-// Crypto Asset Tickers API documentation//
+
+// Crypto Asset Ticker API documentation//
 var settings = {
 	"async": true,
 	"crossDomain": true,
@@ -61,4 +64,3 @@ var settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
-
