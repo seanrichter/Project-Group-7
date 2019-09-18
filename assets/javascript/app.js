7e22f5dfad779f740acc50ae17826d0fcd2af3f8
Sean Richter <sean.richter04@outlook.com>
2019-09-17 19:04:59 -0500

    3rd api addded and working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,22 @@
 
-// Yahoo Finance API Documentation//
-/*var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://investors-exchange-iex-trading.p.rapidapi.com/stock/msft/effective-spread",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "investors-exchange-iex-trading.p.rapidapi.com",
-		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-}); */
-
-// yahoo financials api documentation 
+// Currency converter API Documentation
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=AMRN",
+	"url": "https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=EUR&amount=1",
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-host": "currency-converter5.p.rapidapi.com",
 		"x-rapidapi-key": "7b1446c9edmshf8adfa5f76f3cc9p15e989jsn47a76e99940a"
 	}
 }
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
+	console.log(response.base_currency_name);
+	console.log(response.rates.EUR.currency_name);
+	console.log(response.rates.EUR.rate_for_amount);
 });
-
-/////////////////////////////////column break/////////////////////////////////////////////////////////////////////////
 
 // Investors Exchange API Documentation//
 var settings = {
@@ -46,6 +31,7 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
+	console.log(response);
 	console.log(response.quote.latestPrice);
 	console.log(response.quote.week52High);
 	console.log(response.quote.week52Low);
@@ -71,6 +57,7 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
+	console.log(response);
 	console.log(response.coin_name);
 	console.log(response.last_price);
 	console.log(response.price_24hr_pcnt);
